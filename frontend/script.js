afdb45ebe2c25f96a88cb1cb72e50bf14335d9dd
Jakucs Viktor <jakucs42@gmail.com>
2026-03-09 15:47:18 +0100

    sorted and display countries

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,22 @@
 // Add your Javascript code here
 let URL = "https://jvvkjy8utk.execute-api.eu-central-1.amazonaws.com/tourist/api/countries/all"
-
+let ul = document.getElementById("ul")
 
 async function fetchData(url){
-    fetch(url)
-    let res = await res.json()
-    let data = await 
+    let res = await fetch(url)
+    let data = await res.json()
+    return data
 }
 
-fetchData(URL)
+async function displayCountries(){
+    let datas = await fetchData(URL)
+    console.log(datas)
+    let sortedDatas = datas.sort((a, b)=> a.name.common.localeCompare(b.name.common))
+    for(let country of sortedDatas){
+        let li = document.createElement("li")
+        li.textContent = `${country.name.common} (${country.capitals[0]})`
+        ul.appendChild(li)
+    }
+}
+
+displayCountries()
