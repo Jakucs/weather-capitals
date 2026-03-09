2f14ccd020f2acbb8649813d5d8cf20e79954c01
Jakucs Viktor <jakucs42@gmail.com>
2026-03-09 11:02:43 +0100

    initial commit

---
+++ frontend/script.js
@@ -0,0 +1,11 @@
+// Add your Javascript code here
+let URL = "https://jvvkjy8utk.execute-api.eu-central-1.amazonaws.com/tourist/api/countries/all"
+
+
+async function fetchData(url){
+    fetch(url)
+    let res = await res.json()
+    let data = await 
+}
+
+fetchData(URL)
