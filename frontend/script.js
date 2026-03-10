b0dc9a37fe1e1ce55e6d0c4d7819301d2e1e6888
Jakucs Viktor <jakucs42@gmail.com>
2026-03-10 20:51:32 +0100

    add btn listener

--- frontend/script.js
+++ frontend/script.js
@@ -4,6 +4,9 @@ let api_key = "9f470b0b7f1440689e693427260903"
 let root = document.getElementById("root")
 let ul = document.getElementById("ul")
 
+// let prev = document.getElementById("prev")
+// let next = document.getElementById("next")
+
 async function fetchData(){
     let res = await fetch(URL)
     let data = await res.json()
@@ -19,6 +22,7 @@ async function displayCountries(){
         let li = document.createElement("li")
         li.textContent = `${country.name.common} (${country.capitals[0]})`
         li.id = index++
+        li.classList.add("mainLI")
         ul.appendChild(li)
     }
 }
@@ -30,14 +34,19 @@ async function fetchOne(city, api_key){
     return data
 }
 
-async function listElementEventListener(){
+async function eventListeners(){
     root.addEventListener("click", function(e){
         //console.log("e.target", e.target)
-        
+        if(e.target.classList.contains("mainLI")){
             fetchOne(e.target.textContent, api_key).then(data=>displayWeather(e.target, data))
-        
+        }
+
+        if(e.target.classList.contains("btn")){
+            fetchOne(e.target.textContent, api_key).then(data=>displayWeather(e.target, data))
+        }
     })
 }
+
 
 function displayWeather(target, data){
         console.log("target", target)
@@ -45,18 +54,23 @@ function displayWeather(target, data){
         if(enabledDiv){
             return
         }
+        if(document.querySelector(".temp-enabled")){
+            document.querySelector(".temp-enabled").remove()
+        }
+
         let div = document.createElement("div")
         div.classList.add("temp-enabled")
         let span = document.createElement("span")
         span.textContent = `Temperature: ${data.current.temp_c}C°`
         span.enabled = true
         div.appendChild(span)
+
+        
         target.appendChild(div)
+
         let nextElement = target.nextElementSibling
-        console.log(nextElement)
-        
-        
+        console.log(nextElement)   
 }
 
 displayCountries()
-listElementEventListener()
+eventListeners()
